7a9250075018616292949c2e85c8cd960b14b06e
amydady <215952449@qq.com>
2018-10-22 14:55:28 +0800

    NONE

--- common/config.js
+++ common/config.js
@@ -4,20 +4,24 @@
 
 //http请求相关配置
 var CommonConfig = {
-    httpReqConfig: {
-        headers: { 'Content-Type': 'application/json' }
-    }
+
 };
 
-CommonConfig.configApp = function(app){
+CommonConfig.configApp = function (app) {
     app.config(['$locationProvider', function ($locationProvider) {
         $locationProvider.html5Mode(
-          {
-            enabled: true,
-            requireBase: false//必须配置为false，否则<base href=''>这种格式带base链接的地址才能解析
-          });
-      }]);
+            {
+                enabled: true,
+                requireBase: false//必须配置为false，否则<base href=''>这种格式带base链接的地址才能解析
+            });
+    }]);
 };
 
-var debugFlag = true
+CommonConfig.httpReqConfig = {
+    headers: { 'Content-Type': 'application/json' }
+}
 
+// Rest接口基路径定义
+CommonConfig.RestBaseUrl = {
+    CaoBaoService: 'http://localhost:8006/rest/littlecat/caobao/'
+};
