d2d7ddd77fab14eb2a807e1ee6c55edca794dedf
amydady <215952449@qq.com>
2018-10-23 00:00:52 +0800

    NONE

--- common/AlertUtil.js
+++ common/AlertUtil.js
@@ -4,15 +4,16 @@
 
 
 var AlertUtil = {
-
-
 };
 
-AlertUtil.dangerWithCloseButton = function (targetElemId, content) {
+// 默认的告警消失时间
+AlertUtil.defaultTimeout = 4000;
+
+AlertUtil.alertWithCloseButton = function (type, targetElemId, content, timeout) {
     var targetElement = document.getElementById(targetElemId);
 
     var alertStr = " \
-    <div class=\"alert alert-danger alert-dismissable\"> \
+    <div class=\"alert alert-"+ type + " alert-dismissable\"> \
     <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\"> \
       &times; \
     </button> "
@@ -20,48 +21,28 @@ AlertUtil.dangerWithCloseButton = function (targetElemId, content) {
         "</div>";
 
     targetElement.innerHTML = alertStr;
+
+    if (timeout != null && timeout != "undefined") {
+        window.setTimeout(function () {
+            $('[data-dismiss="alert"]').alert('close');
+        }, timeout);
+    }
 };
 
-AlertUtil.infoWithCloseButton = function (targetElemId, content) {
-    var targetElement = document.getElementById(targetElemId);
-
-    var alertStr = " \
-    <div class=\"alert alert-info alert-dismissable\"> \
-    <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\"> \
-      &times; \
-    </button> "
-        + content +
-        "</div>";
-
-    targetElement.innerHTML = alertStr;
+AlertUtil.dangerWithCloseButton = function (targetElemId, content, timeout) {
+    AlertUtil.alertWithCloseButton('danger', targetElemId, content, timeout);
 };
 
-AlertUtil.successWithCloseButton = function (targetElemId, content) {
-    var targetElement = document.getElementById(targetElemId);
-
-    var alertStr = " \
-    <div class=\"alert alert-success alert-dismissable\"> \
-    <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\"> \
-      &times; \
-    </button> "
-        + content +
-        "</div>";
-
-    targetElement.innerHTML = alertStr;
+AlertUtil.infoWithCloseButton = function (targetElemId, content, timeout) {
+    AlertUtil.alertWithCloseButton('info', targetElemId, content, timeout);
 };
 
-AlertUtil.warningWithCloseButton = function (targetElemId, content) {
-    var targetElement = document.getElementById(targetElemId);
+AlertUtil.successWithCloseButton = function (targetElemId, content, timeout) {
+    AlertUtil.alertWithCloseButton('success', targetElemId, content, timeout);
+};
 
-    var alertStr = " \
-    <div class=\"alert alert-warning alert-dismissable\"> \
-    <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\"> \
-      &times; \
-    </button> "
-        + content +
-        "</div>";
-
-    targetElement.innerHTML = alertStr;
+AlertUtil.warningWithCloseButton = function (targetElemId, content, timeout) {
+    AlertUtil.alertWithCloseButton('warning', targetElemId, content, timeout);
 };
 
 AlertUtil.clearAlert = function (targetElemId) {
